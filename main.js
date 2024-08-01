27bdde5990edc9626f0da1986726a658fd24fd1b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-08-01 19:28:43 +0600

    Added todo on database

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import express from "express";
+import { Todo } from "./models/Todo.js";
 const app = express();
 const port = 5000;
 
-let a = await mongoose.connect(
+let con = await mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tx9lkv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 );
 
 app.get("/", (req, res) => {
+  const todo = new Todo({
+    title: 1,
+    desc: "Description of this Todo",
+    isDone: true,
+  });
+  todo.save();
   res.send("Hello World!");
 });
 
