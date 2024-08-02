4e5af2e84725712fa70d2297f1ac5745bfa19b91
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-08-02 15:33:02 +0600

    Initial learning completed

--- main.js
+++ main.js
@@ -9,18 +9,17 @@ let con = await mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tx9lkv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 );
 
-// app.get("/", (req, res) => {
-//   const todo = new Todo({
-//     title: 1,
-//     desc: "Description of this Todo",
-//     isDone: true,
-//     days: 3,
-//   });
-//   todo.save();
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  const todo = new Todo({
+    desc: "Description of this Todo",
+    isDone: true,
+    days: 3,
+  });
+  todo.save();
+  res.send("Hello World!");
+});
 
-app.get("/", async (req, res) => {
+app.get("/a", async (req, res) => {
   let todo = await Todo.findOne({});
   res.json({ title: todo.title, desc: todo.desc });
 });
